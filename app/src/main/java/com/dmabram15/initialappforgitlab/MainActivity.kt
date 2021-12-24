b16a78ed3feee3b0c681cd8190bc020978005b14
d.abramovskih <dab@colos.studio>
2021-12-24 18:51:17 +0300

    not shows heads up notification

--- app/src/main/java/com/dmabram15/initialappforgitlab/MainActivity.kt
+++ app/src/main/java/com/dmabram15/initialappforgitlab/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.dmabram15.initialappforgitlab
 
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    val notificationReceiver = NotificationReceiver()
+    lateinit var startBtn: Button
+
+    lateinit var alarmManager : AlarmManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+
+        startBtn = findViewById<Button>(R.id.start_btn)
+        startBtn.setOnClickListener { onStartBtnClick() }
+    }
+
+
+
+    fun onStartBtnClick() {
+        val intent = Intent(this, NotificationReceiver::class.java).apply {
+            putExtra(NotificationReceiver.EXTRA_KEY_MESSAGE, "Some message")
+        }
+        val pendingIntent = PendingIntent.getBroadcast(applicationContext, 101, intent, PendingIntent.FLAG_ONE_SHOT)
+
+        registerReceiver(notificationReceiver, IntentFilter(Intent.ACTION_USER_PRESENT))
+
+        alarmManager
+            .setExact(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + 5000, pendingIntent)
     }
 }
--- app/src/main/java/com/dmabram15/initialappforgitlab/NotificationReceiver.kt
+++ app/src/main/java/com/dmabram15/initialappforgitlab/NotificationReceiver.kt
@@ -1,4 +1,72 @@
 package com.dmabram15.initialappforgitlab
 
-class NotitficatioReceiver {
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.os.Build
+import androidx.core.app.NotificationCompat
+
+class NotificationReceiver : BroadcastReceiver() {
+
+    override fun onReceive(p0: Context?, p1: Intent?) {
+
+        p0?.run{ pushNotification(p0) }
+
+    }
+
+    private fun pushNotification(context: Context) {
+        val intent = Intent(context, MainActivity::class.java)
+        val nm = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val pendingIntent = PendingIntent.getActivity(context, 42, intent, PendingIntent.FLAG_ONE_SHOT)
+        var notification: Notification
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+        {
+            nm.createNotificationChannel(
+                NotificationChannel("some_notification_channel", "My_app", NotificationManager.IMPORTANCE_HIGH).apply {
+                    description = "Some channel"
+                    lockscreenVisibility = Notification.VISIBILITY_PRIVATE
+                    setShowBadge(true)
+                }
+            )
+
+            notification = Notification.Builder(context, "some_notification_channel")
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setColor(context.getColor(R.color.cardview_light_background))
+                .setContentIntent(pendingIntent)
+                .setTicker("Some ticker")
+                .setContentTitle("Some title")
+                .setContentText("SomeText")
+                .setLargeIcon(BitmapFactory.decodeResource(context.resources, R.drawable.ic_launcher_foreground))
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setCategory(Notification.CATEGORY_EVENT)
+                .build()
+        }
+        else
+        {
+            notification = NotificationCompat.Builder(context, "some_notification_channel")
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setColor(context.getColor(R.color.cardview_light_background))
+                .setContentIntent(pendingIntent)
+                .setContentTitle("Some title")
+                .setContentText("SomeText")
+                .setTicker("Some ticker")
+                .setLargeIcon(BitmapFactory.decodeResource(context.resources, R.drawable.ic_launcher_foreground))
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setCategory(Notification.CATEGORY_EVENT)
+                .build()
+        }
+
+        nm.notify(100, notification)
+    }
+
+    companion object {
+        val EXTRA_KEY_MESSAGE = "message_key"
+    }
+
 }
